3ffa7f76e3b23bd1d98003bb009501aaff7f8e8e
Jake Ortega <jacob.ortega@lunatech.nl>
2026-01-08 19:39:52 +0100

    feat: Increase Cloud Run memory allocation to 32Gi and reduce default fps to 24

--- src/defaultSpec.ts
+++ src/defaultSpec.ts
@@ -4,7 +4,7 @@ export const DEFAULT_SPEC: RenderSpec = {
   config: {
     width: 1080,
     height: 1920,
-    fps: 30,
+    fps: 24, // Reduced from 30 to 24 fps (20% fewer frames)
     durationInFrames: 300,
   },
   assets: {},
